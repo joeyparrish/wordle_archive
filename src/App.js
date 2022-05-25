04d10d323c108ba870923dc62a2dfd7eb55b39c9
Joey Parrish <joey.parrish@gmail.com>
2022-05-25 15:02:10 -0700

    Move Board component to own file

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { ReactComponent as Info } from './data/Info.svg'
 import { ReactComponent as Settings } from './data/Settings.svg'
 import { ReactComponent as Share } from './data/Share.svg'
 
+import { Board } from './components/Board'
 import { InfoModal } from './components/InfoModal'
 import { SettingsModal } from './components/SettingsModal'
 import { EndGameModal } from './components/EndGameModal'
@@ -38,94 +39,6 @@ function computeAnswer(gameId) {
   }
 
   return answer;
-}
-
-class Board extends React.Component {
-  getCellStyles(rowIdx, colIdx, letter) {
-    if (rowIdx === this.props.currentRow) {
-      if (letter) {
-        return [
-          'nm-inset-background',
-          'dark:nm-inset-background-dark',
-          'text-primary',
-          'dark:text-primary-dark',
-          this.props.submittedInvalidWord ? 'border border-red-800' : '',
-        ].join(' ');
-      } else {
-        return [
-          'nm-flat-background',
-          'dark:nm-flat-background-dark',
-          'text-primary',
-          'dark:text-primary-dark',
-        ].join(' ');
-      }
-    }
-
-    switch (this.props.cellStatuses[rowIdx][colIdx]) {
-      case status.green:
-        if (this.props.colorBlindMode) {
-          return [
-            'nm-inset-orange-500',
-            'text-gray-50',
-          ].join(' ');
-        } else {
-          return [
-            'nm-inset-n-green',
-            'text-gray-50',
-          ].join(' ');
-        }
-
-      case status.yellow:
-        if (this.props.colorBlindMode) {
-          return [
-            'nm-inset-blue-300',
-            'text-gray-50',
-          ].join(' ');
-        } else {
-          return [
-            'nm-inset-yellow-500',
-            'text-gray-50',
-          ].join(' ');
-        }
-
-      case status.gray:
-        return [
-          'nm-inset-n-gray',
-          'text-gray-50',
-        ].join(' ');
-
-      default:
-        return [
-          'nm-flat-background',
-          'dark:nm-flat-background-dark',
-          'text-primary',
-          'dark:text-primary-dark',
-        ].join(' ');
-    }
-  }
-
-  render() {
-    return (
-      <div className="flex items-center flex-col py-4">
-        <div className="grid grid-cols-5 grid-flow-row gap-4">{
-          this.props.board.map((row, rowIdx) => (
-            row.map((letter, colIdx) => (
-              <span
-                key={colIdx}
-                className={[
-                  `${this.getCellStyles(rowIdx, colIdx, letter)}`,
-                  'inline-flex items-center font-medium justify-center text-xl',
-                  'w-[14vw] h-[14vw] xs:w-14 xs:h-14 sm:w-20 sm:h-20 rounded',
-                ].join(' ')}
-              >
-                {letter}
-              </span>
-            ))
-          ))
-        }</div>
-      </div>
-    );
-  }
 }
 
 export default class App extends React.Component {
