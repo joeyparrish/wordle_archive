06d210c215309db91cb418ad93697d6b764230e9
Joey Parrish <joey.parrish@gmail.com>
2022-05-25 09:25:37 -0700

    Fix hidden text in info modal on short screens

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -6,7 +6,7 @@ Modal.setAppElement('#root')
 
 export const InfoModal = ({ isOpen, handleClose, darkMode, colorBlindMode, styles }) => (
   <Modal isOpen={isOpen} onRequestClose={handleClose} style={styles} contentLabel="Game Info Modal">
-    <div className={`h-full ${darkMode ? 'dark' : ''}`}>
+    <div className={`${darkMode ? 'dark' : ''}`}>
       <button
         className="absolute top-4 right-4 rounded-full nm-flat-background dark:nm-flat-background-dark text-primary dark:text-primary-dark p-1 w-6 h-6 sm:p-2 sm:h-8 sm:w-8"
         onClick={handleClose}
